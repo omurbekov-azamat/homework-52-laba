b10d2f5d9be42c1d107d7a2be8ce2b9e3440fb8b
omurbekov-azamat <avgusttin8@gmail.com>
2022-10-22 17:08:56 +0600

    commit-2 level-1 Done!

---
+++ src/CardView/CardView.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface CardViewProps {
+  rank: string;
+  suit: string;
+}
+
+const CardView: React.FC<CardViewProps> = (props) => {
+  const changeClass = (rank: string, suit: string) => {
+    let rankChange;
+    let suitChange;
+
+    const ranks: string[] = ['2', '3', '4', '5', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    for (let i = 0; i < ranks.length; i++) {
+      if (rank === ranks[i]) {
+        rankChange = rank.toLowerCase();
+      }
+    }
+
+    if (suit === '♦') {
+      suitChange = 'diams';
+    } else if (suit === '♥') {
+      suitChange = 'hearts';
+    } else if (suit === '♣') {
+      suitChange = 'clubs';
+    } else {
+      suitChange = 'spades';
+    }
+
+    return 'card rank-' + rankChange + ' ' + suitChange;
+  }
+
+  return (
+    <div className="playingCards faceImages">
+      <span className={changeClass(props.rank, props.suit)}>
+
+    <span className="rank">{props.rank}</span>
+    <span className="suit">{props.suit}</span>
+
+    </span>
+    </div>
+  );
+};
+
+export default CardView;
+
